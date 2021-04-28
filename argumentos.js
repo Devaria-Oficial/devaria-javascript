42b4164033f30ecc632e5e6eda9643ce8f8bc548
Rafael Thomazelli <rafael@verzel.com.br>
2021-04-27 21:44:48 -0300

    parte 3 aula 27/04/21

--- argumentos.js
+++ argumentos.js
@@ -1,5 +1,5 @@
-let nome = process.argv[2];
-let idade = process.argv[3];
+const nome = process.argv[2];
+const idade = process.argv[3];
 
 console.log("o nome e", nome);
 console.log("a idade e", idade);
